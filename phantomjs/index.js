a7b7f0e98266d9ddd2a4a5b5de8387acf6066aef
Shady <shadyzekry75@gmail.com>
2021-04-02 02:23:04 +0200

    Add scrapping basics

---
+++ phantomjs/index.js
@@ -0,0 +1,45 @@
+const puppeteer = require("puppeteer");
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  // const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setViewport({ height: 1080, width: 1920 });
+  await page.goto(
+    "https://web.vodafone.com.eg",
+    {
+      waitUntil: "networkidle2",
+    }
+  );
+
+  await page.click("button[id=InnerloginBtn]");
+  await sleep(4000);
+
+  await page.focus("#username");
+  await page.keyboard.type("YourPhoneNumber");
+  await page.focus("#password");
+  await page.keyboard.type("YourPassword");
+
+  // await page.waitFor("input[name=login]");
+  await page.waitForFunction(
+    'document.querySelector("#submitBtn").className ==="btn btn-primary card-btn js-btn-submit2"'
+  );
+  await page.click("input[name=login]");
+
+  await sleep(10000);
+
+  page.on("response", async (response) => {
+    if (response.url().includes("services/dxl/usage/usageConsumptionReport"))
+      console.log(JSON.stringify(await response.json(), null, 2));
+  });
+
+  await page.click("a[id=maintab-DSL-link]");
+  await sleep(3000);
+  await page.screenshot({ path: "screenshot.png" });
+
+  await browser.close();
+})();
