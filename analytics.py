10fecb51dd69b67e7c184b88a97ac60c37c3a2a2
Shady <shadyzekry75@gmail.com>
2021-04-06 02:01:36 +0200

    Show final usage details

--- analytics.py
+++ analytics.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 import sys
 import json
-import datetime
+from datetime import datetime
 
 resultJson = json.loads(sys.argv[1])
 
-usageString = resultJson[0]["bucket"][0]["bucketBalance"][0]["remainingValue"]["amount"]
+usageString = resultJson[0]["bucket"][0]["bucketCounter"][0]["value"]["amount"]
 validForString = resultJson[0]["bucket"][0]["bucketBalance"][0]["validFor"]["endDateTime"].split('T')[0]
-remainingString = resultJson[0]["bucket"][0]["bucketCounter"][0]["value"]["amount"]
+remainingString = resultJson[0]["bucket"][0]["bucketBalance"][0]["remainingValue"]["amount"]
 
-#expirationDate = datetime.datetime.strptime(validForString, '%Y-%m-%d %H:%M:%S.%f')
-#expirationDate = datetime.datetime.strptime(validForString, 'YYYY-MM-DDThh:mm:ss.sTZD')
-expirationDate = datetime.datetime.strptime(validForString, '%Y-%M-%d')
+#Main data fetched
+usage = int(usageString) / 1024
+remaining = int(remainingString) / 1024
+expirationDate = datetime.strptime(validForString, '%Y-%m-%d')
 
-print(int(usageString))
-print(expirationDate)
-print(int(remainingString))
+#Helper data
+remainingDays = (expirationDate - datetime.now()).days
+
+print("Used GBs:\t\t%.2f" % usage, "GB")
+print("Expiration date:\t", expirationDate)
+print("Remaining GBs:\t\t%.2f" % remaining, "GB")
+print("Days left:\t\t", remainingDays, "Day(s)")
